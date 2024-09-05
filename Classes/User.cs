102a063bdf1c151998b1188a82f7dc7f36172675
Gus <Gus@DESKTOP-1NMG4AP>
2024-09-05 17:25:47 +0300

    Добавьте файлы проекта.

---
+++ Classes/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.Classes
+{
+    public class User
+    {
+        public User(string name, int age) 
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp3.Classes;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    Console.WriteLine("Введите кол-во добавляемых пользователей");
+    int z = int.Parse(Console.ReadLine());
+    List<User> users = new List<User>();
+    for (int x = 0; x < z; x++ )
+    {
+        Console.WriteLine("Как зовут пользователя?");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Возраст пользователя?");
+        int Age = Convert.ToInt32(Console.ReadLine());
+
+        User user = new User(Name, Age);
+
+        users.Add(user);
+    }
+
+    
+}
